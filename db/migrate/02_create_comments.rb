14acf2c0037bd2d3281c03ca2d7c758a2f66ed94
azbury <acasbury25@aol.com>
2020-04-07 17:32:43 -0400

    adding comments migration

---
+++ db/migrate/02_create_comments.rb
@@ -0,0 +1,11 @@
+class CreateComments < ActiveRecord::Migration[4.2]
+    def change
+        create_table :comments do |t|
+            t.text :description
+            t.integer :user_id
+            t.integer :remedy_id
+
+            t.timestamps null: false
+        end
+    end
+end
