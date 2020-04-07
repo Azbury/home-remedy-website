dfc34fbedb8c7030f524848981b16c00f72f3189
azbury <acasbury25@aol.com>
2020-04-07 17:34:52 -0400

    adding remedys migration

---
+++ db/migrate/03_create_remedys.rb
@@ -0,0 +1,11 @@
+class CreateRemedys < ActiveRecord::Migration[4.2]
+    def change
+        create_table :remedys do |t|
+            t.text :description
+            t.integer :user_id
+            t.integer :category_id
+
+            t.timestamps null: false
+        end
+    end
+end
