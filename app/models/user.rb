248ba0f3b9f8bea52fb5dd92ff87049a3ef0b2f0
azbury <acasbury25@aol.com>
2020-04-10 21:24:12 -0400

    fixing associations between models and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
     has_secure_password
-    has_many :remedys
-    has_many :remedys, :through => :comments
+    has_many :remedies
+    has_many :comments
+    has_many :commented_remedies, :through => :comments, source: :remedy
 end
