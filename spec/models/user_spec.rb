a4aadbe59ae7ed33901f41d654b27eb2ebd91ac6
azbury <acasbury25@aol.com>
2020-04-12 09:50:58 -0400

    taking out comment

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -69,13 +69,4 @@ RSpec.describe User, :type => :model do
     expect(new_user.remedies.last).to eq(new_remedy2)
   end
 
-  #it "has a method 'mood' that returns 'sad' when the user is more nauseous than happy" do
-   # expect(user.mood).to eq("sad")
-  #end
-
- # it "has a method 'mood' that returns 'happy' when the user is more happy than nauseous" do
-  #  user.update(:happiness => 7)
-   # expect(user.mood).to eq("happy")
-  #end
-
 end
