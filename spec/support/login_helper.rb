59972723805645353f35c295d7aa7d4895813e14
azbury <acasbury25@aol.com>
2020-04-20 19:26:16 -0400

    deleting dead code

--- spec/support/login_helper.rb
+++ spec/support/login_helper.rb
@@ -16,19 +16,6 @@ module LoginHelper
       click_button('Sign In')
     end
   
-    def admin_signup
-      fill_in("user[name]", :with => "Walt Disney")
-      fill_in("user[password]", :with => "password")
-      find(:css, "#user_admin").set(true)
-      click_button('Create User')
-    end
-  
-    def admin_login
-      select 'Walt Disney',from:'user_name'
-      fill_in("password", :with => "password")
-      click_button('Sign In')
-    end
-  
     def create_standard_user 
       @aimbot =  User.create(
         username: "Aimbot",
@@ -37,22 +24,6 @@ module LoginHelper
         password: "password",
         age: 22,
         bio: "I like to code, play games, and have fun."
-      )
-    end
-  
-    def create_standard_and_admin_user
-      @mindy = User.create(
-        name: "Mindy",
-        password: "password",
-        happiness: 3,
-        nausea: 2,
-        tickets: 10,
-        height: 50
-      )
-      @walt = User.create(
-        name: "Walt Disney",
-        password: "password",
-        admin: true
       )
     end
     
