e3da5a83e9ef1461e22aa52aeb0b2c6ebb3527f4
azbury <acasbury25@aol.com>
2020-04-20 22:51:20 -0400

    adding validations for category attributes

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ActiveRecord::Base
     has_many :remedy_categories
     has_many :remedies, :through => :remedy_categories
+    validates :name, uniqueness: true
+    validates :name, presence: true
 end
