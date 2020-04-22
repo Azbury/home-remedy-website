04fc9243694e9887ed233d985b315fabf441215e
azbury <acasbury25@aol.com>
2020-04-22 10:58:55 -0400

    adding show page for category

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,4 @@ class Category < ActiveRecord::Base
     has_many :remedy_categories
     has_many :remedies, :through => :remedy_categories
     validates :name, uniqueness: true
-    validates :name, presence: true
 end
