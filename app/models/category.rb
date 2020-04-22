aae6f21a1a0d7253a344f947e7a4d3e7c299ae1b
azbury <acasbury25@aol.com>
2020-04-22 11:09:20 -0400

    taking elder tag off user show page

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,5 @@ class Category < ActiveRecord::Base
     has_many :remedy_categories
     has_many :remedies, :through => :remedy_categories
     validates :name, uniqueness: true
+    validates :name, presence: true
 end
