24714f1785d24825d067049ff452c1912e3c2e5c
azbury <acasbury25@aol.com>
2020-04-23 12:39:40 -0400

    creating comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,20 @@
 class CommentsController < ApplicationController
+    def new
+        @comment = Comment.new(remedy_id: params[:remedy_id], user_id: params[:user_id])
+    end
 
+    def create
+        @comment = Comment.new(comment_params)
+        if @comment.save
+            redirect_to user_remedy_path(id: @comment.remedy_id, user_id: @comment.user_id)
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def remedy_params
+        params.require(:comment).permit(:content, :user_id, :remedy_id)
+    end
 end
