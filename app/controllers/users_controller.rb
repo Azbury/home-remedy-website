6d089be57abf0b40d3555ff1098dc8918568b51e
azbury <acasbury25@aol.com>
2020-04-25 15:27:31 -0400

    adding edit method in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,15 +24,23 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-          session[:user_id] = @user.id
-          redirect_to @user
+            session[:user_id] = @user.id
+            redirect_to @user
         else
-          render :new
+            render :new
         end
     end
 
     def elderly
         @elderly = User.elderly
+    end
+
+    def edit
+        if params[:id].to_i != session[:user_id]
+            redirect_to user_path(current_user), notice: "Can only edit your own account."
+        else
+            @user = User.find(params[:id])
+        end
     end
 
     private
