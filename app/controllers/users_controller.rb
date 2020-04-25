9b460aae60efd1bb5eab5418ee2966da559356b2
azbury <acasbury25@aol.com>
2020-04-25 15:29:10 -0400

    adding update method in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,6 +43,12 @@ class UsersController < ApplicationController
         end
     end
 
+    def update
+        @user = User.find(params[:id])
+        @user.update(user_params)
+        redirect_to user_path(@user)
+    end
+
     private
 
     def user_params
