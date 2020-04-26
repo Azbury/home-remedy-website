e3cc74ffeaab4c429005d655bfa6eeca51d812c0
azbury <acasbury25@aol.com>
2020-04-26 12:14:21 -0400

    adding comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,15 +4,18 @@ class ApplicationController < ActionController::Base
     helper_method :current_user
   
     private
-  
+
+    #if user is not logged in will redirect to home page
     def verified_user
       redirect_to '/' unless user_is_authenticated
     end
-  
+    
+    #validates current user
     def user_is_authenticated
       !!current_user
     end
-  
+    
+    #returns current user
     def current_user
       User.find_by(id: session[:user_id])
     end
